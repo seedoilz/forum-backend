c89562397341feb6a498b8fba6b260fb2df8e359
yuhaofeng <2087953253@qq.com>
2023-12-20 15:14:55 +0800

    添加mongodb

--- src/test/java/com/wanted/project/PostTest.java
+++ src/test/java/com/wanted/project/PostTest.java
@@ -15,22 +15,22 @@ public class PostTest extends Tester{
     @Autowired
     private PostDao postDao;
 
-    @Test
-    public void test1(){
-        List<String> tags = new ArrayList<>();
-        tags.add("抽象");
-        tags.add("逆天");
-        tags.add("无聊");
-        Post post = Post.builder()
-                .userId(0L)
-                .anony(false)
-                .createdAt(new Date())
-                .content("第二次发帖啊")
-                .tags(tags).build();
-        postDao.insert(post);
-        List<Post> res = postDao.selectAll();
-        res.forEach(System.out::println);
-    }
+//    @Test
+//    public void test1(){
+//        List<String> tags = new ArrayList<>();
+//        tags.add("抽象");
+//        tags.add("逆天");
+//        tags.add("有趣");
+//        Post post = Post.builder()
+//                .userId(0L)
+//                .anony(false)
+//                .createdAt(new Date())
+//                .content("第一次发帖")
+//                .tags(tags).build();
+//        postDao.insert(post);
+//        List<Post> res = postDao.selectAll();
+//        res.forEach(System.out::println);
+//    }
 
     @Test
     public void test2(){
